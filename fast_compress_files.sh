7ec22492af4292c358e488b3a536c2998a64b9d9
SuhasSrinivasan <32346517+SuhasSrinivasan@users.noreply.github.com>
2022-05-12 20:13:22 -0700

    Change GitHub spaces per tab to 4

--- fast_compress_files.sh
+++ fast_compress_files.sh
@@ -12,38 +12,38 @@ if [ $# == 0 ] ; then
     exit 1;
 fi
 
-find "$1" -type f '(' -iname '*.adj'					\
+find "$1" -type f '(' -iname '*.adj'		\
 				-o -iname '*.bed'			\
-				-o -iname '*.bedGraph' 			\
-				-o -iname '*.broadPeak' 		\
+				-o -iname '*.bedGraph' 		\
+				-o -iname '*.broadPeak' 	\
 				-o -iname '*.bt2' 			\
 				-o -iname '*.bt2l' 			\
 				-o -iname '*.bw' 			\
-				-o -iname '*.cgbins' 			\
-				-o -iname '*.cgfreq' 			\
+				-o -iname '*.cgbins' 		\
+				-o -iname '*.cgfreq' 		\
 				-o -iname '*.csv' 			\
 				-o -iname '*.fa' 			\
 				-o -iname '*.faa' 			\
 				-o -iname '*.fna' 			\
-				-o -iname '*.fasta' 			\
-				-o -iname '*.fastq' 			\
-				-o -iname '*.gappedPeak'		\
+				-o -iname '*.fasta' 		\
+				-o -iname '*.fastq' 		\
+				-o -iname '*.gappedPeak'	\
 				-o -iname 'Genome'			\
 				-o -iname '*.gff' 			\
 				-o -iname '*.gtf' 			\
 				-o -iname '*.hic' 			\
 				-o -iname '*.locs' 			\
-				-o -iname '*.matrix'			\
-				-o -iname '*.narrowPeak'		\
+				-o -iname '*.matrix'		\
+				-o -iname '*.narrowPeak'	\
 				-o -iname '*.out'			\
-				-o -iname '*.Rdata' 			\
+				-o -iname '*.Rdata' 		\
 				-o -iname 'SA' 				\
-				-o -iname 'SAindex' 			\
+				-o -iname 'SAindex' 		\
 				-o -iname '*.sam'  			\
 				-o -iname '*.seq' 			\
-				-o -iname '*.tab' 	 		\
+				-o -iname '*.tab' 			\
 				-o -iname '*.tsv' 	 		\
 				-o -iname '*.txt' 	 		\
 				-o -iname '*.xls' 	 		\
-				-o -iname '*.xlsx' ')' 			\
+				-o -iname '*.xlsx' ')' 		\
 		-exec pigz -p4 -v {} ';'
